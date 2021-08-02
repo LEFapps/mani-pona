c22b562e4226b4208d36b1f1a00e0705dbced045
johanlef <jb@eenwereldmetlef.be>
2021-08-02 15:49:44 +0200

    Remove mock client

--- src/client/ManiError.js
+++
@@ -1,6 +0,0 @@
-export default class ManiError extends Error {
-  constructor (message) {
-    super(message)
-    this.name = 'ManiError'
-  }
-}
--- src/client/index.js
+++
@@ -1,6 +0,0 @@
-import MockClient from './MockClient'
-import ManiClient from './ManiClient'
-import ManiError from './ManiError'
-import mani from '../mani'
-
-export { ManiClient, MockClient, ManiError, mani, mani as MANI }
