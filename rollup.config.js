ef1142b4d43ff3c2346b110305b0e54115b14454
johanlef <jb@eenwereldmetlef.be>
2022-03-31 16:45:04 +0200

    Oops (Balance → balance)

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,15 @@
 // import hashbang from 'rollup-plugin-hashbang'
+import json from '@rollup/plugin-json'
 
-export default [{
-  input: 'src/lambda/handler.js',
-  output: {
-    file: 'dist/server.js',
-    format: 'cjs'
+export default [
+  {
+    input: 'src/lambda/handler.js',
+    output: {
+      file: 'dist/server.js',
+      format: 'cjs'
+    },
+    plugins: [json()]
   }
-}
   /*
   , {
   input: 'cli/index.js',
